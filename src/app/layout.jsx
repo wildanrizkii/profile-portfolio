4bd1289194442d42f4a3dad649afde4e043c5dfb
Wildan <wildanrizki9560@gmail.com>
2025-02-11 00:24:15 +0700

    add viewport

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,12 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import CustomCursor from "@/components/CustomCursor";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta-sans",
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"], // Tambahkan sesuai kebutuhan
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -17,6 +18,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
       <body className={`${plusJakartaSans.variable} antialiased`}>
         <CustomCursor />
         {children}
